e006319d5aca6432bb0ca9c145167000581cf6d3
Peter Vlugter <59895+pvlugter@users.noreply.github.com>
2023-10-25 15:37:01 +1300

    fix: require bash script plugin when packaging (#29)

---
+++ src/main/scala-sbt-1.0/com/lightbend/sbt/javaagent/Dependencies.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright © 2016-2023 Lightbend, Inc. <http://www.lightbend.com>
+ */
+
+package com.lightbend.sbt.javaagent
+
+object Dependencies {
+  val BashStartScriptPlugin = "com.typesafe.sbt.packager.archetypes.scripts.BashStartScriptPlugin"
+}
